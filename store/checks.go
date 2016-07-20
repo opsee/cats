bdd853629fdab3e1122e094b92b5ea8cbba0b1a5
Dan Compton <dan@opsee.com>
2016-07-20 15:53:41 -0400

    ok

--- store/checks.go
+++ store/checks.go
@@ -149,7 +149,7 @@ func (q *checkStore) GetCheckCount(user *schema.User, prorated bool) (float32, e
 func (q *checkStore) GetCheckStateTransitionLogEntries(checkId, customerId string, from, to time.Time) ([]*checks.StateTransitionLogEntry, error) {
 	var logEntries []*checks.StateTransitionLogEntry
 
-	err := sqlx.Select(q, &logEntries, "SELECT * FROM check_state_transitions WHERE check_id=$1 AND customer_id=$2 AND created_at BETWEEN $3::timestamp AND $4::timestamp", checkId, customerId, from.Format("2013-06-05T14:10:43.678Z"), to.Format("2013-06-05T14:10:43.678Z"))
+	err := sqlx.Select(q, &logEntries, "SELECT * FROM check_state_transitions WHERE check_id=$1 AND customer_id=$2 AND created_at BETWEEN $3 AND $4", checkId, customerId, from, to)
 	if err != nil {
 		return nil, err
 	}
