e3f63295d3e103db17df66404bde8140aa85d0a0
Mark Martin <assplecake@gmail.com>
2016-07-28 08:16:21 -0700

    add da dang elb

--- terraform/sluice.tf
+++ terraform/sluice.tf
@@ -21,6 +21,7 @@ resource "aws_ecs_service" "sluice" {
     desired_count = 1
     deployment_maximum_percent = 200
     deployment_minimum_healthy_percent = 100
+    iam_role = "${var.ecs_iam_role}"
     
     load_balancer {
         elb_name = "sluice"
